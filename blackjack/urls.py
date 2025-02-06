982939ef584bd702f140e906d7bbffa357c3ddd3
BenMccray <113936697+BenMccray@users.noreply.github.com>
2025-02-06 14:34:41 -0500

    removed redudant directory

--- blackjack/urls.py
+++ blackjack/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("start/", include('start.urls')),
-    path("", RedirectView.as_view(url="/start/")),
+    # path("start/", include('start.urls')),
+    # path("", RedirectView.as_view(url="/start/")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
